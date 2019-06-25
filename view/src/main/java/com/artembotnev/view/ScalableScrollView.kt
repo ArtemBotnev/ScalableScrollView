f1921e9df8ab126980e67c50afb49377c37cbd88
ArtemBotnev <arbotnev7@gmail.com>
2019-06-25 13:37:45 +0300

    added pre draw listener

--- view/src/main/java/com/artembotnev/view/ScalableScrollView.kt
+++ view/src/main/java/com/artembotnev/view/ScalableScrollView.kt
@@ -7,8 +7,11 @@ import android.util.Log
 import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.ScaleGestureDetector
+import android.view.ViewTreeObserver
 import android.widget.ScrollView
 import android.widget.TextView
+import kotlin.math.max
+import kotlin.math.min
 
 open class ScalableScrollView : ScrollView {
 
@@ -16,20 +19,29 @@ open class ScalableScrollView : ScrollView {
     private var maxScale = MAX_SCALE
     private var textView: TextView? = null
 
-    constructor(context: Context) : super(context) {
-        adjustTextView()
-    }
+    constructor(context: Context) : super(context)
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
         context.theme.obtainStyledAttributes(
             attrs, R.styleable.ScalableScrollView, 0, 0
         ).run {
             maxScale = getFloat(R.styleable.ScalableScrollView_maxScale, MAX_SCALE).also {
-                Math.min(it, MAX_SCALE)
+                min(it, MAX_SCALE)
+            }
+        }
+    }
+
+    init {
+        val preDrawListener = object : ViewTreeObserver.OnPreDrawListener {
+            override fun onPreDraw(): Boolean {
+                viewTreeObserver.removeOnPreDrawListener(this)
+                adjustTextView()
+
+                return true
             }
         }
 
-        adjustTextView()
+        viewTreeObserver.addOnPreDrawListener(preDrawListener)
     }
 
     private val textScaleListener by lazy {
@@ -41,7 +53,7 @@ open class ScalableScrollView : ScrollView {
                 val textSize = textView!!.textSize
 
                 scaleFactor *= detector.scaleFactor
-                scaleFactor = Math.max(1.0f, Math.min(scaleFactor, maxScale))
+                scaleFactor = max(1.0f, min(scaleFactor, maxScale))
 
                 textView!!.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize * scaleFactor)
 
@@ -57,6 +69,7 @@ open class ScalableScrollView : ScrollView {
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(ev: MotionEvent) =
         if (ev.pointerCount > 1) scaleText(ev) else super.onTouchEvent(ev)
+
 
     private fun scaleText(ev: MotionEvent): Boolean {
         if (textView == null) return false
